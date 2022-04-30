1db3557aff1376a5cdbc79280293596393b27085
D-Shapovalov <shapovalov-84@internet.ru>
2022-05-01 00:18:26 +0700

    start project

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+  $('.slider').slick({
+    dots: true,
+    autoplay: true,
+    prevArrow: '<button class="slider__btn slider__btn-prev"><img src="img/icons/arrow-left.svg" alt="arrov-prev"></button>',
+    nextArrow: '<button class="slider__btn slider__btn-next"><img src="img/icons/arrow-right.svg" alt="arrov-next"></button>'
+  })
+});
